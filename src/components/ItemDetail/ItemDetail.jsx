88a28afee4afd98d907fb45b6202ea66ec12ff63
limbaniafagalde <32376938+limbaniafagalde@users.noreply.github.com>
2021-07-16 06:39:33 -0300

    ItemDetail Updated

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -10,19 +10,20 @@ const min = 1;
             <div className = "singleProduct">
 
                 <div className="productDetImage">
-                    <h3 className="productTitle">{itemD.title}</h3>
+
                     <img className = "productImg" src={itemD.img} alt={itemD.title} />
                 </div>
 
                 <div className="productDetInfo">
+                    <h3 className="productTitle">{itemD.title}</h3>
                     <p className="productDetDet">
-                        <span>
-                        {itemD.detail}</span>
+                        <span>{itemD.detail}</span>
                     </p>
-                    <p>
+                    <p className="productPrice">
                         <span>{itemD.price}</span> EUR
                     </p>
                     <ItemCount stock = {stock} minimum = {min}/>
+                    <button className="btnAddCart">ADD TO CART</button>
                 </div>
             </div>
         </>

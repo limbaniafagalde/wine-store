5dcb6ba065230a1b9b42f49e000d1652ff5db0ee
limbaniafagalde <32376938+limbaniafagalde@users.noreply.github.com>
2021-07-27 09:02:52 -0300

    CartContext Updated

--- src/components/CartContext/CartContext.jsx
+++ src/components/CartContext/CartContext.jsx
@@ -7,10 +7,19 @@ export const CartProvider = ({children}) => {
     
     const [cart, setCart] = useState([]);
 
-    const addToCart = (item) => {
-    setCart([...cart, item]);
+    //item = item recibido
+    const isInCart = (item) => cart.filter(it => it.id === item.id).length === 0;
 
-    }
+    const addToCart = (item) => {
+            if (isInCart(item)) {
+                setCart([...cart, item]);
+            }
+            else{
+                alert("The item is already added.");
+            }
+        }
+
+    
 
 
     const deleteProduct = (id) =>{
@@ -25,11 +34,12 @@ export const CartProvider = ({children}) => {
         return cart.reduce((acc, prod) => acc +  prod.counter, 0)
     }
 
+    const clearCart = () => setCart([]);
+
     return(
 
         <CartContext.Provider value={{cart, addToCart, totalPrice, totalQuantity, deleteProduct}}>
         {children}
     </CartContext.Provider>
     )
-
 }
